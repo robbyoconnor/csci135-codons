43692058662f8efc9087becd650b253451ef8ecb
robbyoconnor <robby.oconnor@gmail.com>
2012-07-30 06:11:50 -0400

    add the implementation of this class -- untested but it's pretty straightforward

--- RNATranscriber.cc
+++ RNATranscriber.cc
@@ -7,5 +7,28 @@ Modifications:
 ********************************************************************************/
 #include "RNATranscriber.h"
 
+using namespace std;
+
 RNATranscriber::RNATranscriber(){
+}
+
+RNATranscriber::RNATranscriber(string dnaSequence) {
+	this->dnaSequence = dnaSequence;
+}
+
+string RNATranscriber::getRNASequence() {
+	return this->rnaSequence;
+}
+
+void RNATranscriber::transcribe() {
+	this->rnaSequence = this->dnaSequence;
+	unsigned int i = 0;
+	unsigned int size = (unsigned)this->rnaSequence.size();
+	for(;i<size;i++) {
+		if(this->rnaSequence[i]=='t') {
+			this->rnaSequence[i]='u';
+		} else if(this->rnaSequence[i]=='T') {
+			this->rnaSequence[i] = 'U';
+		}
+	}
 }
--- RNATranscriber.h
+++ RNATranscriber.h
@@ -1,29 +1,40 @@
 /******************************************************************************
-Title: RNATranscriber.h
-Author: Robert O'Connor
-Created on: July 22, 2012
-Description: Acts as a means to encapsulate and abstract the act of transcribing.
-Modifications:
-********************************************************************************/
+ Title: RNATranscriber.h
+ Author: Robert O'Connor
+ Created on: July 22, 2012
+ Description: Acts as a means to encapsulate and abstract the act of transcribing.
+ Modifications:
+ ********************************************************************************/
 #ifndef RNA_TRANSCRIBER_H
 #define	RNA_TRANSCRIBER_H
 
 #include <string>
-#include <vector>
 
-#include "RNATranscriber.h"
+class RNATranscriber {
+	public:
+		/**
+		 * Default constructor
+		 */
+		RNATranscriber();
 
-class RNATranscriber{
-  public:
-    RNATranscriber();
+		/**
+		 * Simply sets the dna sequence, after this no other access is given to this class member.
+		 */
+		RNATranscriber(std::string dnaSequence);
 
-    RNATranscriber(std::string dnaSequence);
+		/**
+		 * Returns the RNA Sequence
+		 */
+		std::string getRNASequence();
 
-    void getRNASequence();
+		/**
+		 * Transcribes the DNA sequence to RNA.
+		 */
+		void transcribe();
 
-  private:
-    std::string dnaSequence; // not exposed once set
-    std::string rna_sequence; // is exposed.
+	private:
+		std::string dnaSequence; // not exposed once set
+		std::string rnaSequence; // is exposed.
 };
 
 #endif
